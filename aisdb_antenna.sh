da5ba4a9c460e0098fb65205ea31c9c1f688169b
Jinnkunn <i@jinkunchen.com>
2025-02-25 14:08:46 -0400

    enable local backup
    Signed-off-by: Jinnkunn <i@jinkunchen.com>

--- aisdb_antenna.sh
+++ aisdb_antenna.sh
@@ -8,9 +8,22 @@
 # # the --server-addr option may be repeated in ExecStart multiple times,
 # # see `mproxy-client --help` for more info
 # SERVER_ADDRESS="aisviz.cs.dal.ca:9921"
+
+echo -e "================================================"
+echo -e "🚀 \033[32mAISDB Antenna Setup\033[0m 🚀"
+echo -e "   Version 1.0.0"
+echo -e "================================================"
+
 DEFAULT_SERVER="aisviz.cs.dal.ca:9921"
-read -p $'\033[32mEnter server address (default: '"$DEFAULT_SERVER"$'): \033[0m' SERVER_INPUT
+read -p $'\033[32mEnter server address the singal will be sent to (default: '"$DEFAULT_SERVER"$'): \033[0m' SERVER_INPUT
 SERVER_ADDRESS=${SERVER_INPUT:-$DEFAULT_SERVER}
+
+# Prompt for backup interval
+read -p $'\033[32mEnter number of days to keep data locally (default: 0, no backup): \033[0m' BACKUP_DAYS
+BACKUP_PARAM=""
+if [ ! -z "$BACKUP_DAYS" ] && [ "$BACKUP_DAYS" -gt 0 ] 2>/dev/null; then
+    BACKUP_PARAM="--backup-interval $BACKUP_DAYS"
+fi
 
 set -e
 
@@ -27,7 +40,6 @@ source $HOME/.cargo/env
 # install AISDB dispatch client
 CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse cargo install --git https://github.com/AISViz/mproxy-dispatcher mproxy-client
 
-
 # install the receiver service
 [[ ! -f $HOME/ais_rcv.service ]] && cat <<EOF > "$HOME/ais_rcv.service"
 [Unit]
@@ -38,7 +50,7 @@ Documentation=https://aisviz.gitbook.io/documentation/default-start/ais-hardware
 [Service]
 Type=simple
 User=$USER
-ExecStart=$HOME/.cargo/bin/mproxy-client --path /dev/ttyACM0 --server-addr $SERVER_ADDRESS
+ExecStart=$HOME/.cargo/bin/mproxy-client --path /dev/ttyACM0 --server-addr $SERVER_ADDRESS $BACKUP_PARAM
 Restart=always
 RestartSec=30
 
@@ -53,5 +65,6 @@ sudo systemctl daemon-reload
 sudo systemctl enable ais_rcv
 sudo systemctl start ais_rcv
 
-echo 'Installed AISDB dispatch client' 
-echo "Installed AISDB systemd service to $HOME/ais_rcv.service"
+echo -e '🍺 \033[32mInstalled AISDB dispatch client\033[0m'
+echo -e "🍺 \033[32mInstalled AISDB systemd service to $HOME/ais_rcv.service\033[0m"
+echo -e "✅ \033[32mAISDB Antenna is now running\033[0m"
